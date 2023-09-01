6ab181856d22e6f013a69e38732459e8b35e2742
Syntaxerror2 <70806570+Syntaxerror2@users.noreply.github.com>
2023-09-01 16:26:42 +0200

    Update cart.js

--- front/JS/cart.js
+++ front/JS/cart.js
@@ -1,30 +1,35 @@
-let test1 = localStorage.kanap;
-console.log(test1);
-
-// Ce qui m'a débloqué : la ligne de code suivante puis la boucle, qui m'a permis de récupérer l'ID pour fetch.
+// On récupère le contenu du localStorage
 let copyOfLS = JSON.parse(localStorage.getItem("kanap"));
-console.log(copyOfLS);
+console.log(copyOfLS)
+ 
+// On indique au client si son panier est remplis ou vide
+if (copyOfLS == null || copyOfLS.length == 0 ) {
+  document.getElementById("cart__title").innerHTML += `Votre panier est vide`;
+} else {
+  document.getElementById("cart__title").innerHTML += `Votre panier`;
+}
 
+// On affiche dynamiquement le contenu du localStorage
 function displayCart() {
 for (let produit in copyOfLS) {
   let colors = copyOfLS[produit].color;
   let quantity = copyOfLS[produit].quantity;
-  let quantityInCart = copyOfLS.length -1;
   let id = copyOfLS[produit]._id;
-  console.log(id);
+  let quantityInCart = copyOfLS.length * quantity;
+  
+  //On fetch la donnée du prix de l'id correspondante
   fetch(`http://localhost:3000/api/products/${id}`)
   .then((res) => res.json())
-    .then((data) => {
-    console.log(data);
-  /* à modifier */
-    let price = data.map(quantity) *= data.map(price);
-    document.getElementById("totalQuantity").innerHTML = quantityInCart;
-    document.getElementById("totalPrice").innerHTML = data.reduce(price);
+  .then((data) => {
+  let price = data.price;
 
+  // à modifier
+  document.getElementById("totalPrice").innerHTML = price * quantityInCart;
+  document.getElementById("totalQuantity").innerHTML = quantityInCart;
+  //à modifier 
     
-   
-   /* à modifier */
-    document.getElementById("cart__items").innerHTML += `<article class="cart__item" data-id="${id}" data-color="${colors}">
+  //Affichage dynamique du panier
+  document.getElementById("cart__items").innerHTML += `<article class="cart__item" canapeId="${id}" data-color="${colors}">
     <div class="cart__item__img">
       <img src="${data.imageUrl}" alt="${data.altTxt}" />
     </div>
@@ -32,60 +37,58 @@ for (let produit in copyOfLS) {
       <div class="cart__item__content__description">
         <h2>${data.name}</h2>
         <p>${colors}</p>
-        <p>${total + " €"}</p>
+        <p>${price + " €"}</p>
       </div>
       <div class="cart__item__content__settings">
         <div class="cart__item__content__settings__quantity">
           <p>Qté : </p>
-          <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${quantity}">
+          <input type="number" class="itemQuantity" canapeId="${id}" canapeColor=${colors}" name="itemQuantity" min="1" max="100" value="${quantity}">
         </div>
         <div class="cart__item__content__settings__delete">
-          <p class="deleteItem">Supprimer</p>
+          <p class="deleteItem" canapeId="${id}" canapeColor=${colors}">Supprimer</p>
         </div>
       </div>
     </div>
   </article>`;
 
-})
+  //On permet la suppression au click en identifiant l'article à son id et sa couleur
+  function deleteItem(id, color) {
+    let buttons = document.querySelectorAll(".deleteItem");
+    let items = copyOfLS;
+    for (let button of Array.from(buttons)){
+        button.addEventListener("click", () => {
+    for(i = 0; i < items.length; i++) {
+     if(id == items[i][2] && color == items[i][0]) {
+      items.splice(i, 1);
+      localStorage.setItem("kanap", JSON.stringify(items));
+      window.location.reload();
+     }
+    }})}}
+
+// On permet au client de modifier les quantités de canapé dynamiquement
+
+    function changeQuantity(id, color, quantity) {
+      let change = document.querySelectorAll(".itemQuantity");
+      let items = copyOfLS;
+      for(let changes in Array.from(change)) {
+           changes.addEventListener("click", () => {
+            for (let i = 0; i < items.length; i++) {
+              if (id == items[i][2] && color == items[i][0]) {
+                items[i][1] = quantity;
+              }
+              localStorage.setItem("kanap", JSON.stringify(items));
+              window.location.reload();
+            }})}}
+changeQuantity();
 
 
-  
+
+deleteItem();  
+
+}) 
 }};
 
-function removeItem() {
-  let kanapDelete = document.querySelectorAll(".deleteItem");
-  // just une variable pour stocker mon bouton
-  kanapDelete.forEach((kanape) => {
-  // la méthode forEach permet d'éxécute une fonction une fois par élément du tableau
-    kanape.addEventListener("click", function(e) {
-      let basketValue = copyOfLS;
-      console.log(basketValue);
-      //Là je ne suis pas sûr que le basketvalue soit ma copie du LS
-      const deleteID = e.target.closest("article").getAttribute("data-id");
-      const deleteColor = e.targent.closest("article").getAttribute("data-color");
-      const searchDeleteKanap = basketValue.find(element) = element.id == deleteID && element.colors == deleteColor;
-      // The find() method returns the first element in the provided array that satisfies the provided testing function.
-     
-      //S'inspirer de sa méthode (forEach, target.closest et .find
-      //pour récuper les id) et couleurs
-      // Regarder demain la vidéo en entier
-    }
-   
-  )});
-  
-}
-
-removeItem();
-// D'abord : supprimer un seul élément du localStorage
-
-
-// Faire le total : nombre d'article x son prix x articles au total.
-// Reste à offrir la possibilité de delete des articles (clear une partie du localstorage)
-// Récupérer et vérifier les données utilisateur
-// Rajouter les catch/errors
-// Faire des messages d'alerte
-//La méthode removeItem() de l'interface Storage , lorsque vous lui passez une clé en argument, va supprimer la ressource avec le nom de clé correspondant du storage.
- 
+displayCart();
 
 
 
