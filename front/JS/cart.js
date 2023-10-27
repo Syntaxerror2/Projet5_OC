4bcbcf6b25c383f4c1344f51adfd2a417685ea9e
Syntaxerror2 <70806570+Syntaxerror2@users.noreply.github.com>
2023-10-27 17:24:09 +0200

    Update cart.js
    Changement des quantités (enfin) opérationel

--- front/JS/cart.js
+++ front/JS/cart.js
@@ -20,7 +20,6 @@ for (let produit in copyOfLS) {
   fetch(`http://localhost:3000/api/products/${id}`)
   .then((res) => res.json())
   .then((data) => {
-  console.log(data);
 
   //Affichage dynamique du panier
   document.getElementById("cart__items").innerHTML += `<article class="cart__item" data-id="${id}" data-color="${colors}">
@@ -45,35 +44,30 @@ for (let produit in copyOfLS) {
     </div>
   </article>`;
 
-    function changeQuantity() {
-      let input = document.querySelectorAll(".itemQuantity")
-      let items = copyOfLS;
-      for(quantityKanap of input) {
-        quantityKanap.addEventListener("change", () => {
-          let dataId = quantityKanap.closest(".cart__item").getAttribute("data-id");
-          let dataColor = quantityKanap.closest(".cart__item").getAttribute("data-color");
-          let newQuantity = Number(quantityKanap.value);
-          console.log(newQuantity);
-          console.log(dataId);
-          console.log(dataColor);
-          if (newQuantity > 0 && newQuantity <= 100) {
-            for(let i = 0; i < items.length; i++) {
-              let localStorageId = items[i]._id;
-              let localStorageColor = items[i].color;
-              console.log(localStorageId);
-              console.log(localStorageColor);
-              if(items[i]._id === dataId && items[i].color === dataColor) {
-              // Problème : dans mes consoles.log, il confond les id des produits en stock
-              items[i].quantity = newQuantity;
-              console.log(items[i].quantity)
-              }}
-          localStorage.setItem("kanap", JSON.stringify(copyOfLS));
-          location.reload();
-          }});}}
-    changeQuantity();
-    
+// On permet la modification des quantités en identifiant l'article à son id et sa couleur
+ function changeQuantity() {
+  let input = document.querySelectorAll(".itemQuantity");
+  let items = copyOfLS;
+  for (let quantityKanap of input) {
+    quantityKanap.addEventListener("change", (event) => {
+      let dataId = event.target.getAttribute("data-id");
+      let dataColor = event.target.getAttribute("data-color");
+      let newQuantity = Number(event.target.value);
+      if (newQuantity > 0 && newQuantity <= 100) {
+        for (let i = 0; i < items.length; i++) {
+          if (dataId === items[i]._id && dataColor === items[i].color) {
+            items[i].quantity = newQuantity;
+            localStorage.setItem("kanap", JSON.stringify(copyOfLS));
+          }
+        }
+      }
+    });
+  }
+}
+changeQuantity()
 
-  //On permet la suppression au click en identifiant l'article à son id et sa couleur
+
+  // On permet la suppression au click en identifiant l'article à son id et sa couleur
    function deleteItem() {
     let buttons = document.querySelectorAll(".deleteItem");
     let items = copyOfLS;
@@ -82,19 +76,14 @@ for (let produit in copyOfLS) {
     let dataId = del.closest(".cart__item").getAttribute("data-id");
     let dataColor = del.closest(".cart__item").getAttribute("data-color"); 
     for(let i = 0; i < items.length; i++) {
-      console.log(dataId) // Donne toujours le résultat du dernier de la boucle
-      console.log(dataColor) // Donne toujours le résultat du dernier de la boucle
     if(items[i]._id === dataId && items[i].color === dataColor) {
         items.splice(i, 1);
-      }
-    }
+      }}
     localStorage.setItem("kanap", JSON.stringify(items));
-    window.location.reload();
+    /*window.location.reload();*/
     if(items.length == 0) {
       localStorage.removeItem("kanap");
-    }
-      });
-    }
+    }});}
     return;
   }
   deleteItem();
@@ -119,7 +108,6 @@ displayTotalPrice();
 
 
 displayCart();
-
 
  
        
